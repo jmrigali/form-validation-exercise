8e80cbb17487e352f4ad6bd9ff1c7ab3bb4927bb
jmrigali <jmrigali@ymail.com>
2017-10-12 09:23:44 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//Add JS validation
+//1. add a submit event on the form
+//2. add a verification on email
+//3. Require the first and last name
+//4. append error messages to the DOM if input is not valid
+//STRETCH: Use a blur event on field of your choice and add error handling
